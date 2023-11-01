ee9d72770b8686d8bb83c5267cb416eca7c44d3c
yanlinjiang <deutschyanlin@gmail.com>
2023-11-01 23:29:50 +0100

    Use anonymous functions

---
+++ Robust_new/untitled2.m
@@ -0,0 +1,28 @@
+entries_pf{1} = 1:Nbus; 
+entries_pf{2} = 1:Nbus;
+entries_pf{3} = 1:Nbus;
+entries_pf{4} = 1:Nbus;
+entries_pf{5} = 1:Nbranch;
+entries_pf{6} = 1:Nbranch;
+entries_pf{7} = 1:Nbranch;
+entries_pf{8} = 1:Nbranch;
+entries_pf{9} = 1:4;
+entries_pf{10} = 1:Nbus; %g_u_pinj
+entries_pf{11} = 1:Nbus;
+entries_pf{12} = 1:Nbus;
+entries_pf{13} = 1:Nbus;
+entries_pf{14} = 1:Nbranch; % g_u_vvsin
+entries_pf{15} = 1:Nbranch;
+entries_pf{16} = 1:Nbranch;
+entries_pf{17} = 1:Nbranch;
+entries_pf{18} = 1:Nbus; % g_u_vv
+entries_pf{19} = 1:Nbus;
+entries_pf{20} = 1:Nbranch;
+entries_pf{21} = 1:Nbranch;
+entries_pf{22} = 1:Nbranch;
+entries_pf{23} = 1:Nbranch;
+entries_pf{24} = 1:Nbus;
+entries_pf{25} = 1:Nbus; % Psi_l_vv
+
+
+
